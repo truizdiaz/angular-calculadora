48608b515e97c85fc9963bcb3d7043bc15ca51f6
Tomas Ruiz Diaz <tomiruizdiaz@gmail.com>
2022-01-04 10:16:36 -0300

    some code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Calculator';
+  operandoActual: string = '';
+  operandoAnterior: string = '';
+
+  operacion(operacion: string) {
+    switch (operacion) {
+      case 'limpiar':
+        this.limpiar();
+        break;
+      case 'eliminar':
+        console.log("eliminar");
+        break;
+      case 'dividir':
+        console.log("dividir");
+        break;
+      case 'multiplicar':
+        console.log("multiplicar");
+        break;
+      case 'sumar':
+        console.log("sumar");
+        break;
+      case 'restar':
+        console.log("restar");
+        break;
+      case 'resultado':
+        console.log("resultado");
+        break;
+      default:
+        console.log("No match");
+        break;
+    }
+  }
+
+  limpiar() {
+    this.operandoActual = '';
+    this.operandoAnterior = '';
+  }
+
+  agregarNumero(numero: string) {
+    if (numero === '.' && this.operandoActual.includes('.')) {
+      return
+    }
+    this.operandoActual = this.operandoActual + numero;
+  }
 }
